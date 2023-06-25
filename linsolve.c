7e0cea83668fc8be8614e4785e740933127f5c76
Sujata Bhowmick & Sethupathy S <66269817+spariv@users.noreply.github.com>
2023-06-25 17:12:10 -0400

    Update linsolve.c

--- linsolve.c
+++ linsolve.c
@@ -6,10 +6,10 @@
  *  Copyright (c) 2020 Sujata Bhowmick
  *
  *  This file is part of SPARIV.
- *
- *  SPARIV is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation, version 3.
+ *  SPARIV is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License as published by 
+ *  the Free Software Foundation; either version 2.1 of the License, or
+ *  (at your option) any later version.
  *
  *  SPARIV is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
