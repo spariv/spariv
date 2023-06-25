894bdeb8530e027c44000005c372441f55d55bf4
Sujata Bhowmick & Sethupathy S <66269817+spariv@users.noreply.github.com>
2023-06-25 17:12:37 -0400

    Update linsolve.c

--- linsolve.c
+++ linsolve.c
@@ -1,11 +1,12 @@
 /* * *
  *
- *  SPARIV - Sparse and Matrix Library in C
+ *  SPARIV - Matrix Library in C
  *
  *  Copyright (c) 2011 Sethupathy Subramanian
  *  Copyright (c) 2020 Sujata Bhowmick
  *
  *  This file is part of SPARIV.
+ *
  *  SPARIV is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU Lesser General Public License as published by 
  *  the Free Software Foundation; either version 2.1 of the License, or
